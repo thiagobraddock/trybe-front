e82a5b974ea4eb3d64db23431b001abd5762207e
Thiago "BRADDOCK" Oliveira <braddock01@hotmail.com>
2023-11-09 16:03:42 -0300

    fix: typo

--- src/components/Sections/News/index.tsx
+++ src/components/Sections/News/index.tsx
@@ -16,7 +16,7 @@ function News() {
               está melhorando a cada dia!
             </h2>
             <p className="wow fadeInUp" data-wow-delay=".6s">
-              A Turma 34 começa com muitas novidades: tecnologias, projetos inovadores e muita, muita prática!
+              Front-end começa com muitas novidades: tecnologias, projetos inovadores e muita, muita prática!
             </p>
           </div>
         </div>
